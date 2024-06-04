6d5c44d9682c540dc8136f1b1dbf637567727915
Zearo Jiguang <zearo_jiguang1@163.com>
2024-06-04 11:49:56 +0800

    create history_command

---
+++ bash/builtins/history_command.h
@@ -0,0 +1,17 @@
+//
+// Created by 86158 on 2024/6/3.
+//
+
+#ifndef ZEARO_BASH_HISTORY_COMMAND_H
+#define ZEARO_BASH_HISTORY_COMMAND_H
+
+/**
+ * @brief history. history -n , check the last command in history file.
+ * @param args char**
+ * @return 1
+ */
+static int zbash_history_command(char** args) {
+
+}
+
+#endif //ZEARO_BASH_HISTORY_COMMAND_H
